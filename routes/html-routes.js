1e6b00367578c6e58f4f3260790d43262769f478
KevinW <bird831205@hotmail.com>
2019-05-26 22:11:25 -0400

    190526 handlebars object render

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -12,52 +12,61 @@ var db = require("../models");
 module.exports = function (app) {
 
     app.get("/", function (req, res) {
-        console.log("linking to index")
-        res.render("index")
+
+        db.Article.find({})
+            .then(function (dbArticle) {
+                console.log(dbArticle)
+
+                res.render("index", { article: dbArticle })
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
     })
 
     app.get("/scrape", function (req, res) {
         // First, we grab the body of the html with axios
-        axios.get("https://starcraft2.com/en-us/news").then(function (response) {
-            // Then, we load that into cheerio and save it to $ for a shorthand selector
-            var $ = cheerio.load(response.data);
+        axios.get("https://starcraft2.com/en-us/news")
+            .then(function (response) {
+                // Then, we load that into cheerio and save it to $ for a shorthand selector
+                var $ = cheerio.load(response.data);
 
-            // Now, we grab every h2 within an article tag, and do the following:
-            $(".blog_card-link").each(function (i, element) {
-                // Save an empty result object
-                var result = {};
+                // Now, we grab every h2 within an article tag, and do the following:
+                $(".blog_card-link").each(function (i, element) {
+                    // Save an empty result object
+                    var result = {};
 
-                // Add the url, headline and summary of every link, and save them as properties of the result object
-                result.url = $(this).attr("href");
-                result.headline = $(this)
-                    .children(".blog_card")
-                    .children(".blog_card-block")
-                    .children(".blog_card-title")
-                    .text();
-                result.summary = $(this)
-                    .children(".blog_card")
-                    .children(".blog_card-block")
-                    .children(".blog_card-text")
-                    .text();
+                    // Add the url, headline and summary of every link, and save them as properties of the result object
+                    result.url = $(this).attr("href");
+                    result.headline = $(this)
+                        .children(".blog_card")
+                        .children(".blog_card-block")
+                        .children(".blog_card-title")
+                        .text();
+                    result.summary = $(this)
+                        .children(".blog_card")
+                        .children(".blog_card-block")
+                        .children(".blog_card-text")
+                        .text();
 
 
-                console.log(result)
+                    console.log(result)
 
-                // Create a new Article using the `result` object built from scraping
-                db.Article.create(result)
-                    .then(function (dbArticle) {
-                        // View the added result in the console
-                        console.log(dbArticle);
-                    })
-                    .catch(function (err) {
-                        // If an error occurred, log it
-                        console.log(err);
-                    });
+                    // Create a new Article using the `result` object built from scraping
+                    db.Article.create(result)
+                        .then(function (dbArticle) {
+                            // View the added result in the console
+                            console.log(dbArticle);
+                        })
+                        .catch(function (err) {
+                            // If an error occurred, log it
+                            console.log(err);
+                        });
+                });
+
+                // Send a message to the client
+                res.send("Scrape Complete");
             });
-
-            // Send a message to the client
-            res.send("Scrape Complete");
-        });
     });
 
 }
